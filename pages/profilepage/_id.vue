ce5db2e5e172bdd09e2483ed7e79524f9d4dbc01
yohei1226 <kundagpwm1226@gmail.com>
2021-09-19 22:20:47 +0900

    profilepageのavatorと背景イメージの実装

--- pages/profilepage/_id.vue
+++ pages/profilepage/_id.vue
@@ -1,4 +1,24 @@
 <template>
+<div>
+  <v-card class="profile-top-image">
+            <v-img
+              src="https://tabi-pocket-image.tabikobo.com/wp-content/uploads/2020/11/17135859/1-1-3.jpg"
+              height="300"
+              class="grey darken-4"
+            ></v-img>
+  </v-card>
+  <v-avatar
+      color="orange"
+      size="240"
+      class="profile-icon"
+    >
+      <!-- <span class="white--text text-h5">62</span> -->
+      <img
+        src="https://cdn.vuetifyjs.com/images/john.jpg"
+        alt="John"
+      >
+    </v-avatar>
+</div>
   
 </template>
 
@@ -9,5 +29,14 @@ export default {
 </script>
 
 <style>
-
+.profile-top-image{
+    position: relative;
+}
+.profile-icon{
+    /* margin-top: 100px; */
+    /* margin-left: 100px; */
+    top: 19%;
+    left: 18%;
+    position: absolute;
+}
 </style>
